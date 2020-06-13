c549e5c55f140d3bcd6d420014968105165c1b83
Olman Francisco Castro Hernández <olcastro@estudiantec.cr>
2020-06-13 00:46:17 -0600

    Reportes
    * Se crea la vista Reporte de pacientes por esto.
    * Se imprime la mista.

--- CoTEC-Map/src/app/hospital-center/pages/reports/reports.component.ts
+++ CoTEC-Map/src/app/hospital-center/pages/reports/reports.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  pageName = 'Reporte';
+  show = false;
+  countryData: any;
+  countrystatistics: any;
+
+  constructor() {
+    this.countryData = [
+      {coutry: 'Costa Rica', infections: 23455566, recovered: 6675775, dead: 345566, actives: 566467},
+      {coutry: 'Costa Rica', infections: 234555566, recovered: 6675775, dead: 345566, actives: 566467},
+      {coutry: 'Costa Rica', infections: 23455566, recovered: 66755775, dead: 345566, actives: 5566467},
+      {coutry: 'Costa Rica', infections: 23455566, recovered: 6675775, dead: 3455566, actives: 566467}
+    ];
+    this.countrystatistics =
+      {infections: 23455566, recovered: 6675775, dead: 345566, actives: 566467};
+  }
 
   ngOnInit(): void {
   }
 
+  // Print the report in a pdf
+  print() {
+    window.print();
+  }
+
+  // Active a state patients report
+  reportState() {
+    this.pageName = 'Reporte de pacientes por estado';
+    this.show = true;
+  }
+
+  // Active a state dead report
+  reportDead() {
+    this.pageName = 'Reporte de muertes y nuevos casos';
+    this.show = true;
+  }
 }
