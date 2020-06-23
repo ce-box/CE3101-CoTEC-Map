bf333c014af885edbd276362b20f9768176e101a
BertaBrenes <bertacaro1996@gmail.com>
2020-06-22 20:08:27 -0600

    [ADD][develop]

--- CocTEC-App/src/app/services/data-base.service.ts
+++ CocTEC-App/src/app/services/data-base.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataBaseService {
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
     
   }
 }
