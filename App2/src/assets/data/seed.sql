462e6ed63ece39228722bf7f50eea2680bf5d836
BertaBrenes <bertacaro1996@gmail.com>
2020-07-02 14:33:37 -0600

    [ADD][sqlite]change schema database

--- App2/src/assets/data/seed.sql
+++ App2/src/assets/data/seed.sql
@@ -1,10 +1,38 @@
-CREATE TABLE IF NOT EXISTS app.medication(Medicamento TEXT, )
-CREATE TABLE IF NOT EXISTS app.patient(DNI INTEGER PRIMARY KEY, Id_Centro_Hospitalario TEXT KEY , N_Pais TEXT KEY,Nombre TEXT,Apellido_1 TEXT,Apellido_2 TEXT, Fecha_nac INTEGER, Estado TEXT, Internado TEXT, UCI TEXT);
+CREATE TABLE IF NOT EXISTS patientStatus(
+    Id INTEGER PRIMARY KEY,
+    Name TEXT KEY,
+);
+CREATE TABLE IF NOT EXISTS patient(
+    Dni TEXT PRIMARY KEY, 
+    Name TEXT KEY,
+    LastName TEXT KEY,
+    BoD NUMERIC KEY,
+    Status INTEGER ,
+    Hospitalized NONE KEY,
+    UCI NONE KEY,
+    Hospital_ID INTEGER,
+    Region TEXT,
+    COUNTRY TEXT,
+    FOREIGN KEY (Status) REFERENCES patientStatus (Id)
+     );
 INSERT or IGNORE INTO patient VALUES (304980524,'Alberto','Brenes Brenes',17,'costarricense', 'Cartago',"['asthmatic', 'hypertensive']", 'ACTIVE', 'Ibuprofen', TRUE,TRUE);
-INSERT or IGNORE INTO patient VALUES (306980824,'Bertha','Brenes Brenes',23,'costarricense', 'Cartago',"['asthmatic', 'hypertensive']", 'ACTIVE', 'Ibuprofen', TRUE,FALSE);
-INSERT or IGNORE INTO patient VALUES (303020181,'Ivannia','Brenes Fernandez',52,'costarricense', 'Cartago',"['asthmatic', 'hypertensive']", 'ACTIVE', 'Ibuprofen', FALSE,FALSE);
  
-CREATE TABLE IF NOT EXISTS app.contact(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, lastName TEXT, age INTEGER, nationality TEXT, address TEXT, pathology TEXT, email TEXT);
+CREATE TABLE IF NOT EXISTS contact(
+    Dni TEXT PRIMARY KEY AUTOINCREMENT,
+    Name TEXT KEY, 
+    LastName TEXT KEY, 
+    DoB NUMERIC KEY, 
+    Region TEXT, 
+    COUNTRY TEXT, 
+    Email TEXT KEY
+);
+
+CREATE TABLE IF NOT EXISTS contact(
+    PatientDni TEXT PRIMARY KEY,
+    ContactDni TEXT PRIMARY KEY,
+    MeetingDate NUMERIC KEY,
+    FOREIGN KEY (PatientDni) REFERENCES patient (Dni),
+    FOREIGN KEY (ContactDni) REFERENCES contact (Dni)
+);
+
 INSERT or IGNORE INTO contact VALUES (302980150,'Marta',  'Brenes Fernandez', 58, 'costarricense', 'tucurrique', "['asthmatic', 'hypertensive']", 'bertacaro1996@gmail.com');
-INSERT or IGNORE INTO contact VALUES (301980150,'Vivian',  'Bermudez Bermudez', 59, 'costarricense', 'tucurrique', "['asthmatic', 'hypertensive']", 'bertacaro1996@hotmail.com');
-INSERT or IGNORE INTO contact VALUES (305980150,'Moncho',  'Brenes Fernandez', 59, 'costarricense', 'tucurrique', "['asthmatic', 'hypertensive']", 'bertacaro1996@yahoo.com');;
